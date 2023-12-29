f621d96c57340da721a5d35a3355f7c579ceb941
Zed Ikejiani <zedikejiani@Zeds-MacBook-Pro.local>
2023-12-29 18:54:36 -0500

    first

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
  //Gund-Zero/
-  base: '/', //formerly just "/"
+  base: '/gundzero/', //formerly just "/"
   plugins: [react()],
   
 })
